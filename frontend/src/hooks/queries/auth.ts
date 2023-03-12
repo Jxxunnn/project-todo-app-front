7a7339d6af504b961817d55036d0d290f6ec8a26
Jxxunnn <roof2586@naver.com>
2023-03-12 16:16:20 +0900

    fix usePostSignUpMuation 훅 수정

--- frontend/src/hooks/queries/auth.ts
+++ frontend/src/hooks/queries/auth.ts
@@ -1,20 +1,10 @@
 import { useMutation, useQueryClient } from "react-query";
 import { useNavigate } from "react-router";
 import { postLogin, postSignup } from "@/api/auth";
-import { QUERY_KEYS, MUTATION_KEYS } from "@/constants/queries";
 import ROUTES from "@/constants/routes";
-import { setToken } from "@/utils/storage";
 
-export const useSignUp = () => {
-  const navigate = useNavigate();
+export const usePostSignUpMutation = () => {
+  const mutation = useMutation(postSignup);
 
-  const { mutate } = useMutation(postSignup, {
-    retry: 0,
-    onSuccess: () => {
-      navigate(ROUTES.TODOS);
-    },
-    mutationKey: MUTATION_KEYS.SIGNUP,
-  });
-
-  return { mutate };
+  return mutation;
 };
